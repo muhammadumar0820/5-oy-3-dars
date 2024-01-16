f3986f643c66ca87b0ea41d5fe62cd358f3ae8af
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2024-01-16 20:02:02 +0300

    vazifa qilindi

---
+++ index.js
@@ -0,0 +1,13 @@
+let card = document.getElementsByClassName("cars");
+let Mercedes = document.getElementsByClassName("mersedesBenz");
+let Tesla = document.getElementsByClassName("teslaT");
+let btn = document.getElementsByClassName("btn");
+Mercedes.style.display = 'flex';
+Tesla.style.display = 'flex';
+btn.addEventListener(click, function(){
+    mersedesBenz.delete;
+    Mercedes.style.display = 'flex';
+});
+btn.addEventListener(click, function(){
+    teslaT.delete;
+});
